f0cccfd0c30224398928c741a04e62220ad0ae70
ravi_yliway <ravi@yliway.com>
2024-04-18 15:54:35 +0530

    stroke issue

--- src/components/pages/products/productDetails/ProductDetail.js
+++ src/components/pages/products/productDetails/ProductDetail.js
@@ -39,16 +39,16 @@ const ProductDetail = () => {
             viewBox="0 0 24.00 24.00"
             xmlns="http://www.w3.org/2000/svg"
             stroke="#000000"
-            stroke-width="0.00024000000000000003"
+            strokeWidth="0.00024000000000000003"
             transform="rotate(0)"
           >
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g
               id="SVGRepo_tracerCarrier"
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke="#CCCCCC"
-              stroke-width="0.144"
+              strokeWidth="0.144"
             ></g>
             <g id="SVGRepo_iconCarrier">
               <path d="M1.293,12.707a1,1,0,0,1,0-1.414l5-5A1,1,0,0,1,7.707,7.707L4.414,11H22a1,1,0,0,1,0,2H4.414l3.293,3.293a1,1,0,1,1-1.414,1.414Z"></path>
