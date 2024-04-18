e9d5c302350013d468ac2c5e1d472873b34988d0
ravi_yliway <ravi@yliway.com>
2024-04-18 15:36:22 +0530

    back to product

--- src/components/pages/products/productDetails/ProductDetail.js
+++ src/components/pages/products/productDetails/ProductDetail.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { TabTitle } from "../../../../utils/GeneralFunctions";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const ProductDetail = () => {
   TabTitle("Product Details");
@@ -31,6 +31,31 @@ const ProductDetail = () => {
     <div className="product_details_main">
       <h1>Product Details</h1>
       <div key={id}>
+        <Link to="/products" className="back_to_product">
+          <svg
+            fill="#000000"
+            width="256px"
+            height="256px"
+            viewBox="0 0 24.00 24.00"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="#000000"
+            stroke-width="0.00024000000000000003"
+            transform="rotate(0)"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke="#CCCCCC"
+              stroke-width="0.144"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path d="M1.293,12.707a1,1,0,0,1,0-1.414l5-5A1,1,0,0,1,7.707,7.707L4.414,11H22a1,1,0,0,1,0,2H4.414l3.293,3.293a1,1,0,1,1-1.414,1.414Z"></path>
+            </g>
+          </svg>
+          Back to products
+        </Link>
         <h2>{product.title}</h2>
         <p>{product.body}</p>
       </div>
