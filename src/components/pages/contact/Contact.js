9120b693c56e733e4d28eaf329523356f0f14982
KashyapThumar <kashyapatel551437@gmail.com>
2024-06-10 09:13:38 +0530

    formik form

--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -1,10 +1,99 @@
 import { TabTitle } from "../../../utils/GeneralFunctions";
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { Button, TextField } from "@mui/material";
+
+const validationSchema = yup.object({
+  name: yup
+    .string()
+    .min(4, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Name is required"),
+  email: yup
+    .string()
+    .email("Enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(8, "Password should be of minimum 8 characters length")
+    .required("Password is required"),
+});
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const initialValues = {
+    name: name,
+    email: email,
+    password: password,
+  };
+
   TabTitle("Contact Us");
+
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <div>
-      <h1>Contact</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          id="name"
+          name="name"
+          label="Name"
+          value={formik.values.name}
+          onChange={(e) => {
+            setName(e.target.value);
+            formik.handleChange(e);
+          }}
+          onBlur={formik.handleBlur}
+          error={formik.touched.name && Boolean(formik.errors.name)}
+          helperText={formik.touched.name && formik.errors.name}
+          sx={{ marginBottom: 1 }}
+        />
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+            formik.handleChange(e);
+          }}
+          onBlur={formik.handleBlur}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+          sx={{ marginBottom: 1 }}
+        />
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+            formik.handleChange(e);
+          }}
+          onBlur={formik.handleBlur}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+          sx={{ marginBottom: 3 }}
+        />
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Submit
+        </Button>
+      </form>
     </div>
   );
 };
